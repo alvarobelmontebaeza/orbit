4fcf9f7f454db520d1726514f122709fcad56036
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-03-08 14:01:38 +0100

    Add TODOs

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/config/tako/velocity_env_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/config/tako/velocity_env_cfg.py
@@ -137,6 +137,7 @@ class ObservationsCfg:
             noise=Unoise(n_min=-0.1, n_max=0.1),
             clip=(-1.0, 1.0),
         )
+        #TODO: Add feet contact observation
 
         def __post_init__(self):
             self.enable_corruption = True
@@ -235,7 +236,7 @@ class RewardsCfg:
         func=mdp.feet_air_time,
         weight=0.125,
         params={
-            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*FOOT"),
+            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*gecko"),
             "command_name": "base_velocity",
             "threshold": 0.5,
         },
@@ -243,7 +244,7 @@ class RewardsCfg:
     undesired_contacts = RewTerm(
         func=mdp.undesired_contacts,
         weight=-1.0,
-        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*THIGH"), "threshold": 1.0},
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*THIGH"), "threshold": 1.0}, #TODO: Modify body names
     )
     # -- optional penalties
     flat_orientation_l2 = RewTerm(func=mdp.flat_orientation_l2, weight=0.0)
@@ -259,6 +260,7 @@ class TerminationsCfg:
         func=mdp.illegal_contact,
         params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names="body"), "threshold": 1.0},
     )
+    # TODO: Add termination if no feet contact
 
 
 @configclass
