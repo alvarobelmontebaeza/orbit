a49a5b245450b61d3b3343a98ad16977e31a8874
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-03-26 17:26:21 +0100

    Setup init scripts

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/__init__.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/__init__.py
@@ -5,49 +5,10 @@
 
 import gymnasium as gym
 
-from . import agents, flat_env_cfg, rough_env_cfg, velocity_env_cfg
+from . import agents, flat_env_cfg, rough_env_cfg
 
 ##
 # Register Gym environments.
 ##
 
-gym.register(
-    id="Isaac-Contrib-Velocity-Flat-Tako-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": flat_env_cfg.TakoFlatEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.TakoFlatPPORunnerCfg,
-        "skrl_cfg_entry_point": "omni.isaac.contrib_tasks.tako.agents:skrl_cfg.yaml",
-    },
-)
 
-gym.register(
-    id="Isaac-Contrib-Velocity-Flat-Tako-Play-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": flat_env_cfg.TakoFlatEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.TakoFlatPPORunnerCfg,
-    },
-)
-
-gym.register(
-    id="Isaac-Contrib-Velocity-Rough-Tako-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": rough_env_cfg.TakoRoughEnvCfg,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.TakoRoughPPORunnerCfg,
-    },
-)
-
-gym.register(
-    id="Isaac-Contrib-Velocity-Rough-Tako-Play-v0",
-    entry_point="omni.isaac.orbit.envs:RLTaskEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": rough_env_cfg.TakoRoughEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": agents.rsl_rl_cfg.TakoRoughPPORunnerCfg,
-    },
-)
