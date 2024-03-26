616c00f8b55122072d2d450dd2dbb3a1154be9a1
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-03-26 17:28:52 +0100

    Extend maximum iterations

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/velocity/config/tako/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/velocity/config/tako/agents/rsl_rl_cfg.py
@@ -46,7 +46,7 @@ class TakoFlatPPORunnerCfg(TakoRoughPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 300
+        self.max_iterations = 1500
         self.experiment_name = "tako_flat"
         self.policy.actor_hidden_dims = [128, 128, 128]
         self.policy.critic_hidden_dims = [128, 128, 128]
