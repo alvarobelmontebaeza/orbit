556484203777874ea01f4605e25381fcfe66d159
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-04-15 13:35:41 +0200

    Modify PPO runner for leg position tracking environment

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/position_control/config/tako/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/position_control/config/tako/agents/rsl_rl_cfg.py
@@ -13,24 +13,24 @@ from omni.isaac.orbit_tasks.utils.wrappers.rsl_rl import (
 
 
 @configclass
-class TakoRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
+class TakoPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 1500
     save_interval = 50
-    experiment_name = "tako_pos_rough"
+    experiment_name = "tako_leg_pos"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
+        actor_hidden_dims=[128, 128, 128],
+        critic_hidden_dims=[128, 128, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
+        entropy_coef=0.01,
+        num_learning_epochs=8,
         num_mini_batches=4,
         learning_rate=1.0e-3,
         schedule="adaptive",
@@ -39,14 +39,3 @@ class TakoRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.01,
         max_grad_norm=1.0,
     )
-
-
-@configclass
-class TakoFlatPPORunnerCfg(TakoRoughPPORunnerCfg):
-    def __post_init__(self):
-        super().__post_init__()
-
-        self.max_iterations = 2000
-        self.experiment_name = "tako_pos_flat"
-        self.policy.actor_hidden_dims = [128, 128, 128]
-        self.policy.critic_hidden_dims = [128, 128, 128]
