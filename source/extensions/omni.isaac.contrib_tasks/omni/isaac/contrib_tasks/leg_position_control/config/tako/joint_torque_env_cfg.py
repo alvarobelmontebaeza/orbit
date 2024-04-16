1d4b03f5a48b43c2e055cf9a20faacc10603b5ff
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-04-16 13:53:00 +0200

    add foot offset for making position commands as offsets of initial foot pos

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/config/tako/joint_torque_env_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/config/tako/joint_torque_env_cfg.py
@@ -30,26 +30,45 @@ class TakoLegPositionControlEnvCfg(LegPositionControlEnvCfg):
         super().__post_init__()
         leg_prefix = "LF"
         foot_name = leg_prefix + "_gecko"
+        # Feet init pos
+        feet_init_pos = {
+            "LF": [0.55, 0.9, -0.5],
+            "LH": [-0.3, 0.9, -0.5],
+            "RF": [0.55, -0.73, -0.5],
+            "RH": [-0.3, -0.73, -0.5],
+        }
 
         self.scene.robot = TAKO_CFG.replace(prim_path="{ENV_REGEX_NS}/tako")
+        self.scene.robot.usd_path = "/home/alvaro/Desktop/tako_leg_pos_control.usd"
+        self.scene.robot.init_state.pos = (0.0, 0.0, 0.0)
         # override events
         self.events.reset_robot_joints.params["position_range"] = (0.75, 1.25)
         # For now, remove randomization events
         self.events.base_external_force_torque = None
-        self.events.reset_base = None
-        self.events.reset_robot_joints = None
         self.events.push_robot = None
         # override rewards
         self.rewards.ee_pos_tracking.params["asset_cfg"].body_names = [foot_name]
         self.rewards.ee_orient_tracking.params["asset_cfg"].body_names = [foot_name]
         # override actions
+        '''
         self.actions.leg_action = mdp.JointEffortActionCfg(
+            asset_name="robot", joint_names=[leg_prefix + ".*"], scale=1.0, 
+        )
+        '''
+        self.actions.leg_action = mdp.JointPositionActionCfg(
             asset_name="robot", joint_names=[leg_prefix + ".*"], scale=0.5
         )
+        
         # override command generator body
         # end-effector is along x-direction
         self.commands.ee_pose.body_name = foot_name
-        self.commands.ee_pose.ranges.pitch = (math.pi / 2, math.pi / 2)
+        self.commands.ee_pose.ranges.pitch = (-math.pi / 4, math.pi / 4)
+
+        # Set command generator to sample points around the foot initial position
+        self.commands.ee_pose.ranges.pos_x = (feet_init_pos[leg_prefix][0] - 0.5, feet_init_pos[leg_prefix][0] + 0.5)
+        self.commands.ee_pose.ranges.pos_y = (feet_init_pos[leg_prefix][1] - 0.5, feet_init_pos[leg_prefix][1] + 0.5)
+        self.commands.ee_pose.ranges.pos_z = (feet_init_pos[leg_prefix][2] - 0.5, feet_init_pos[leg_prefix][2] + 0.5)
+
 
 
 @configclass
