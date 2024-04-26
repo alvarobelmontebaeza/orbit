c9f85895c7350c856a311f6f6d47a47c237c2c17
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-04-26 13:26:35 +0200

    testing script to try the agent in different trajectories

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/scripts/test.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/scripts/test.py
@@ -15,7 +15,7 @@ import argparse
 from omni.isaac.orbit.app import AppLauncher
 
 # local imports
-import cli_args  # isort: skip
+from source.standalone.workflows.rsl_rl import cli_args  # isort: skip
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
@@ -23,7 +23,6 @@ parser.add_argument("--cpu", action="store_true", default=False, help="Use CPU p
 parser.add_argument(
     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
 )
-parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
 # append RSL-RL cli arguments
@@ -41,6 +40,8 @@ simulation_app = app_launcher.app
 import gymnasium as gym
 import os
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 
 from rsl_rl.runners import OnPolicyRunner
 
@@ -58,7 +59,7 @@ def main():
     """Play with RSL-RL agent."""
     # parse configuration
     env_cfg = parse_env_cfg(
-        args_cli.task, use_gpu=not args_cli.cpu, num_envs=args_cli.num_envs, use_fabric=not args_cli.disable_fabric
+        args_cli.task, use_gpu=not args_cli.cpu, num_envs=1, use_fabric=not args_cli.disable_fabric
     )
     agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(args_cli.task, args_cli)
 
@@ -86,27 +87,64 @@ def main():
     export_model_dir = os.path.join(os.path.dirname(resume_path), "exported")
     export_policy_as_onnx(ppo_runner.alg.actor_critic, export_model_dir, filename="policy.onnx")
 
-    # Get trajectory
-    points_x, points_z = generate_circular_trajectory(center=torch.tensor([0.55, -0.5]), radius=0.15, num_points=1000)
-    idx = 0
+    ############# START EXECUTION LOOP #############
 
     # reset environment
-    obs, _ = env.get_observations()
+    obs, _ = env.reset()
+    obs, _, _, _ = env.step(torch.zeros(env.action_space.sample().shape))
+    #obs, _ = env.get_observations()
+
+    # Get trajectory
+    # Configuration
+    traj_time = 10.0 #s
+    dt = env.unwrapped.step_dt
+    tracking_point_update_rate = 10 #steps per update
+    num_points = int((traj_time / dt)/ tracking_point_update_rate) 
+    print(f"Number of points: {num_points}")
+    x0, y0, z0 = obs[0, -12].item(), obs[0, -11].item(), obs[0, -10].item() # initial position
+    radius = 0.2
+    print(f"Initial position: ({x0}, {y0}, {z0})")
+
+    x_plan, z_plan = generate_circular_trajectory(center=torch.tensor([x0-radius, z0]), radius=radius, num_points=num_points+1)
+    #x_plan, z_plan = generate_linear_trajectory_x_axis(torch.tensor([x0, z0]), length=0.2, num_points=num_points+1)
+    # Delete first point since it is the initial position
+    x_plan = x_plan[1:]
+    z_plan = z_plan[1:]
+    x_real = torch.zeros_like(x_plan)
+    z_real = torch.zeros_like(z_plan)
+    idx = 0
+    steps = 0
+
     # simulate environment
     while simulation_app.is_running():
         # run everything in inference mode
         with torch.inference_mode():
             # Override position command
-            obs[:,-19:-16] = torch.tensor([points_x[idx], 0.9, points_z[idx]], device=obs.device)
+            obs[:,-19:-16] = torch.tensor([x_plan[idx], y0, z_plan[idx]], device=obs.device)
             # agent stepping
             actions = policy(obs)
             # env stepping
             obs, _, _, _ = env.step(actions)
 
-            if idx < len(points_x) - 1:
-                idx += 1
-            else:
-                idx = 0
+            if idx < (len(x_plan) - 1):
+                # Store real trajectory
+                if steps % tracking_point_update_rate == 0:
+                    x_real[idx] = obs[0, -12]
+                    z_real[idx] = obs[0, -10]
+                    idx += 1 
+
+            steps = env.unwrapped.common_step_counter
+               
+            if steps == int(traj_time / dt) + tracking_point_update_rate:
+                # Store last point
+                x_real[idx] = obs[0, -12]
+                z_real[idx] = obs[0, -10]
+                
+                print(f"Mean tracking error: {torch.mean(torch.abs(x_plan - x_real))}" )
+                plot_circular_trajectory(x_plan, z_plan, x_real, z_real, torch.tensor([x0, z0]))
+                #plot_linear_trajectory(x_plan, z_plan, x_real, z_real)
+                plot_real_vs_expected(x_plan, z_plan, x_real, z_real, sec_per_point=dt*tracking_point_update_rate)
+                break
 
     # close the simulator
     env.close()
@@ -117,6 +155,50 @@ def generate_circular_trajectory(center, radius, num_points):
     y = center[1] + radius * torch.sin(angles)
     return x, y
 
+def generate_linear_trajectory_x_axis(x0, length, num_points):
+    t = torch.linspace(0, 1, num_points)
+    trajectory = x0 + t.view(-1, 1) * length * torch.tensor([1.0, 0.0])  # Direction along X-axis
+    return trajectory[:, 0], trajectory[:, 1]
+
+def plot_circular_trajectory(x_plan, y_plan, x_real, y_real, center):
+    # Plotting the circular trajectory
+    plt.figure(figsize=(6, 6))
+    plt.plot(x_plan.numpy(), y_plan.numpy(), 'b-')
+    plt.plot(x_real.numpy(), y_real.numpy(), 'r-')
+    plt.plot(center[0].item(), center[1].item(), 'go')  # Plotting the center point
+    plt.axis('equal')
+    plt.title('Circular Trajectory')
+    plt.xlabel('X')
+    plt.ylabel('Z')
+    plt.grid(True)
+    plt.show()
+
+def plot_linear_trajectory(x_plan, y_plan, x_real, y_real):
+    # Plotting the linear trajectory
+    plt.figure(figsize=(6, 6))
+    plt.plot(x_plan.numpy(), y_plan.numpy(), 'b-')
+    plt.plot(x_real.numpy(), y_real.numpy(), 'r-')
+    plt.axis('equal')
+    plt.title('Linear Trajectory')
+    plt.xlabel('X')
+    plt.ylabel('Z')
+    plt.grid(True)
+    plt.show()
+
+def plot_real_vs_expected(x_plan, y_plan, x_real, y_real, sec_per_point=0.05):
+    # Plotting the real vs expected trajectory
+    time = np.arange(1, len(x_plan)+1) * sec_per_point
+    plt.figure(figsize=(6, 6))
+    plt.plot(time, x_plan.numpy(), 'b-')
+    plt.plot(time, x_real.numpy(), 'r-')
+    plt.plot(time, y_plan.numpy(), 'g-')
+    plt.plot(time, y_real.numpy(), 'y-')
+    plt.title('Real vs Planned Trajectory')
+    plt.xlabel('Time')
+    plt.ylabel('Position (m)')
+    plt.legend(['X-Plan', 'X-Real', 'Z-Plan', 'Z-Real'])
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     # run the main function
