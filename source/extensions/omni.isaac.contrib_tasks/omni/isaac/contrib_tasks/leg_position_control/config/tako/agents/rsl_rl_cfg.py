baa10129564031de32c3f30567689bb80daa2c77
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-04-29 12:10:05 +0200

    Add PPO config for one leg environment

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/config/tako/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/config/tako/agents/rsl_rl_cfg.py
@@ -40,3 +40,32 @@ class TakoPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.01,
         max_grad_norm=1.0,
     )
+
+@configclass
+class TakoSimplePPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 45
+    max_iterations = 5000 #1500
+    save_interval = 50
+    experiment_name = "tako_one_leg_pos"
+    resume = False
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[128, 128, 128],
+        critic_hidden_dims=[128, 128, 128],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.035,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-3,
+        schedule="adaptive",
+        gamma=0.99,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
