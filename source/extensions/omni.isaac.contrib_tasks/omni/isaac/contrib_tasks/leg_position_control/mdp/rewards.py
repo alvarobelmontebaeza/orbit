d88535464d6df951250198026ef69034ba19bedf
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-05-03 09:16:21 +0200

    remove clamping from power term computation

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/mdp/rewards.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/mdp/rewards.py
@@ -43,7 +43,7 @@ def joint_power_l2(env: RLTaskEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("r
     """
     # extract the used quantities (to enable type-hinting)
     asset: Articulation = env.scene[asset_cfg.name]
-    power = torch.clamp(asset.data.applied_torque[:, asset_cfg.joint_ids] * asset.data.joint_vel[:, asset_cfg.joint_ids], min=0.0)
+    power = asset.data.applied_torque[:, asset_cfg.joint_ids] * asset.data.joint_vel[:, asset_cfg.joint_ids]
     return torch.sum(torch.square(power), dim=1)
 
 """
@@ -86,7 +86,7 @@ def position_command_error_exp(env: RLTaskEnv, sigma: float, command_name: str, 
     
     return torch.exp(-factor * torch.norm(curr_pos_w - des_pos_w, dim=1))
 
-def orientation_command_error_ln(env: RLTaskEnv, command_name: str, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+def orientation_command_error_ln(env: RLTaskEnv, epsilon: float, command_name: str, asset_cfg: SceneEntityCfg) -> torch.Tensor:
     """Penalize tracking orientation error using shortest path.
 
     The function computes the orientation error between the desired orientation (from the command) and the
@@ -100,7 +100,7 @@ def orientation_command_error_ln(env: RLTaskEnv, command_name: str, asset_cfg: S
     des_quat_b = command[:, 3:7]
     des_quat_w = quat_mul(asset.data.root_state_w[:, 3:7], des_quat_b)
     curr_quat_w = asset.data.body_state_w[:, asset_cfg.body_ids[0], 3:7]  # type: ignore
-    return -torch.log(1.e-5 + quat_error_magnitude(curr_quat_w, des_quat_w))
+    return -torch.log(epsilon + quat_error_magnitude(curr_quat_w, des_quat_w))
 
 def orientation_command_error_exp(env: RLTaskEnv, sigma: float, command_name: str, asset_cfg: SceneEntityCfg) -> torch.Tensor:
     """Penalize tracking orientation error using shortest path.
