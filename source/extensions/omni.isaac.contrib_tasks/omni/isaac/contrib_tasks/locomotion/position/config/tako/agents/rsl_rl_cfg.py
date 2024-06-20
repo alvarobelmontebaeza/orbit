0ab79341903d408d04ff98c3f7742811c20dedd3
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-06-20 11:03:07 +0200

    modify ppo params

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/agents/rsl_rl_cfg.py
@@ -15,22 +15,24 @@ from omni.isaac.orbit_tasks.utils.wrappers.rsl_rl import (
 @configclass
 class TakoRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 1500
-    save_interval = 50
+    max_iterations = 20000
+    save_interval = 100
     experiment_name = "tako_pos_rough"
-    empirical_normalization = False
+    resume = False
+    empirical_normalization = True
     policy = RslRlPpoActorCriticCfg(
+        class_name="ActorCritic", # ActorCriticRecurrent
         init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
+        actor_hidden_dims=[512, 512, 512],
+        critic_hidden_dims=[512, 512, 512],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
+        entropy_coef=0.01,
+        num_learning_epochs=8,
         num_mini_batches=4,
         learning_rate=1.0e-3,
         schedule="adaptive",
@@ -46,7 +48,5 @@ class TakoFlatPPORunnerCfg(TakoRoughPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 2000
+        self.max_iterations = 20000
         self.experiment_name = "tako_pos_flat"
-        self.policy.actor_hidden_dims = [128, 128, 128]
-        self.policy.critic_hidden_dims = [128, 128, 128]
