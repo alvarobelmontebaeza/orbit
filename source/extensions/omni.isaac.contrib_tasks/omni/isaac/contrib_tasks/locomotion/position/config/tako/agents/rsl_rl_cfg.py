7a6b499c592110198979097bfa3496bc68c3a27c
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-07-18 10:46:21 +0200

    Increase training time

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/agents/rsl_rl_cfg.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/locomotion/position/config/tako/agents/rsl_rl_cfg.py
@@ -16,7 +16,7 @@ from omni.isaac.orbit_tasks.utils.wrappers.rsl_rl import (
 class TakoRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 20000
-    save_interval = 100
+    save_interval = 500
     experiment_name = "tako_pos_rough"
     resume = False
     empirical_normalization = True
@@ -48,5 +48,5 @@ class TakoFlatPPORunnerCfg(TakoRoughPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 20000
+        self.max_iterations = 50000
         self.experiment_name = "tako_pos_flat"
