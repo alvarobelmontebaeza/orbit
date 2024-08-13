de99f6785a5f337cb51ece7ae8eb73dc9700ebfc
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-08-13 16:00:52 +0200

    add minimum thruster force to ensure it is never zero and deactivates external forces on the body

--- source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/mdp/actions/base_motion_actions.py
+++ source/extensions/omni.isaac.contrib_tasks/omni/isaac/contrib_tasks/leg_position_control/mdp/actions/base_motion_actions.py
@@ -98,10 +98,9 @@ class BodyThrusterAction(ActionTerm):
     def process_actions(self, actions: torch.Tensor):
         # store the raw actions
         self._raw_actions[:] = actions        
-        # Scale the action by the max force
-        self._processed_actions = self._raw_actions.clone()
         # Clip the actions to the max force
-        self._processed_actions = torch.clip(self._processed_actions, min=-self._max_push_force, max=self._max_push_force)
+        self._processed_actions = torch.tanh(self._raw_actions) * self._max_push_force
+        self._processed_actions = torch.clip(self._processed_actions, min=-self._max_push_force, max=self._max_push_force) + 1e-5
 
     def apply_actions(self):
         # apply forces to each body
